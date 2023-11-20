d40e6ddc9e4d037f35ac39b43031f44370cd169f
abdulqziz2009 <148250194+abdulqziz2009@users.noreply.github.com>
2023-11-20 15:15:54 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BankAccount account = new BankAccount(20000);
+
+        try {
+            while (true) {
+                account.withdraw(6000);
+            }
+        } catch (LimitException e) {
+            System.out.println("Снято " + e.getRemainingAmount() + " сом. На счете осталось " + account.getAmount() + " сом.");
+        }
     }
 }
